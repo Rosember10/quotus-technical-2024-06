0b0a117a2361640f4e4cab642d41a5e652dc86bb
Rosember10 <rosember.01@outlook.com>
2024-06-25 20:02:30 -0400

    feat(routing): add routing feature to IconLink component

--- src/components/utils/IconLink/IconLink.tsx
+++ src/components/utils/IconLink/IconLink.tsx
@@ -4,19 +4,28 @@ import style from './iconlink.module.css'
 import { IconType } from 'react-icons';
 import { useRouter } from 'next/router';
 
-interface IconlinkProps{
-    text:string;
-    Icon: IconType; 
-    select?:boolean
+// Define props for IconLink component
+interface IconlinkProps {
+  text: string;
+  Icon: IconType;
+  select?: boolean;
+  route?: string;
 }
 
-const IconLink = ({text,Icon, select}:IconlinkProps) => {
-  const router = useRouter()
+const IconLink = ({ text, Icon, select, route }: IconlinkProps) => {
+  const router = useRouter();
+  
+   // Handle click event to navigate to the route if provided
+  const handleClick = () => {
+    if (route) {
+      router.push(`${route}`)
+    }
+  }
 
   return (
-    <div className={style.container} onClick={() => router.reload()}>
-         <Icon className={select ? style.svgBlack : style.svg} />
-         <span className={select ? style.boldBlack : style.text}>{text}</span>
+    <div className={style.container} onClick={handleClick}>
+      <Icon className={select ? style.svgBlack : style.svg} />
+      <span className={select ? style.boldBlack : style.text}>{text}</span>
     </div>
   )
 }
