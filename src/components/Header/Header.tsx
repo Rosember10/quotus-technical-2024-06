294023df4b47a4b7ab22c4af65e53628e619c37c
Rosember10 <rosember.01@outlook.com>
2024-06-26 12:30:23 -0400

    feat(ui): add image and background for header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,15 +2,19 @@ import React from 'react'
 import style from './header.module.css'
 import ActionButton from '../utils/ActionButton/ActionButton';
 import { IoIosArrowRoundForward } from "react-icons/io";
+import Image from 'next/image';
 
 
 const Header = () => {
     return (
         <div className={style.container}>
             <h1> Input financial. <br /> Output intelligence. </h1>
-            <p>QUOTUS® transforms your monthly OEM financial statements 
-            into actionable business insights for dealerships and groups. </p>
+            <p>QUOTUS® transforms your monthly OEM financial statements
+                into actionable business insights for dealerships and groups. </p>
             <ActionButton text="Try our demo" Icon={IoIosArrowRoundForward} route='/dashboard' />
+            <div className={style.imageContainer}>
+                <Image src="/images/demo-software.webp" alt='logo quotus' width={900} height={700} priority={true} />
+            </div>
         </div>
     )
 }
